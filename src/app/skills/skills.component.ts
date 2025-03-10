ad7c9a3dc8ac2ce5312545dc02109dc4a2bb8709
Volodymyr1939r <rudenkovolodymyr086@gmail.com>
2025-03-10 10:44:47 +0200

    Remote

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import{CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
+interface Skill {
+  name: string;
+  level: number; // рівень володіння у відсотках
+}
 @Component({
   selector: 'app-skills',
   imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.css'
+  styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
-Skills=[
-  { name:'Adobe Photoshop',level:90 },
-  { name:'Microsoft Word',level:85 },
-  { name:'Adobe Illustrator',level:80 },
-  { name:'Microsoft PowerPoint',level:75 }
-];
+  skillsLeft: Skill[] = [
+    {name: 'Adobe Photoshop', level: 90},
+    {name: 'Microsoft Word', level: 85}
+  ];
+
+  skillsRight: Skill[] = [
+    {name: 'Adobe Illustrator', level: 80},
+    {name: 'Microsoft PowerPoint', level: 75}
+  ];
 }
