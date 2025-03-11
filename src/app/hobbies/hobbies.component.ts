c4750e09085938a2240fd1a050fa8fc6520afe06
Volodymyr1939r <rudenkovolodymyr086@gmail.com>
2025-03-11 02:06:45 +0200

    Remote

--- src/app/hobbies/hobbies.component.ts
+++ src/app/hobbies/hobbies.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
-import{CommonModule} from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-hobbies',
   imports: [CommonModule],
   templateUrl: './hobbies.component.html',
-  styleUrl: './hobbies.component.css'
+  styleUrl: './hobbies.component.scss'
 })
 export class HobbiesComponent {
 hobbies=[
-  {icon:'fa-plane',title:'Travelling'},
-  {icon:'fa-gamepad',title:'Gaming'},
-  {icon:'fa-music',title:'Music'},
-  {icon:'fa-dumbbell',title:'Gym'}
+  {icon: 'fas fa-plane', title: 'Travelling'},
+  {icon: 'fas fa-gamepad', title: 'Gaming'},
+  {icon: 'fas fa-music', title: 'Music'},
+  {icon: 'fas fa-dumbbell', title: 'Gym'}
 ];
 }
